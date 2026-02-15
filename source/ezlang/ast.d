3893dda12764053d1e17016e299aa5232efb0cda
dibyendumajumdar <mobile@majumdar.org.uk>
2026-02-15 12:19:16 +0000

    AST port

--- source/ezlang/ast.d
+++ source/ezlang/ast.d
@@ -370,77 +370,400 @@ class ArrayLoadExpr : Expr {
 } 
 
 class ArrayStoreExpr : Expr {
-
+    Expr _array;
+    Expr _expr;
+    Expr _value;
+    this(Expr array, Expr expr, Expr value) {
+        _array = array;
+        _expr = expr;
+        _value = value;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _array.toStr(sb);
+        sb.put("[");
+        _expr.toStr(sb);
+        sb.put("]=");
+        _value.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _array.accept(visitor);
+        _expr.accept(visitor);
+        _value.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class ArrayInitExpr : ArrayStoreExpr {
-
+    this(Expr array, Expr expr, Expr value) {
+        super(array, expr, value);
+    }
+    override void toStr(ref Appender!string sb) const {
+        _value.toStr(sb);
+    }
 }
 
 class GetFieldExpr : Expr {
-
+    Expr _object;
+    string _fieldName;
+    this(Expr object, string fieldName) {
+        _object = object;
+        _fieldName = fieldName;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _object.toStr(sb);
+        sb.put(".");
+        sb.put(_fieldName);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _object.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class SetFieldExpr : Expr {
-
+    Expr _object;
+    string _fieldName;
+    Expr _value;
+    this(Expr object, string fieldName, Expr value) {
+        _object = object;
+        _fieldName = fieldName;
+        _value = value;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _object.toStr(sb);
+        sb.put(".");
+        sb.put(_fieldName);
+        sb.put("=");
+        _value.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _object.accept(visitor);
+        _value.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class InitFieldExpr : SetFieldExpr {
-
+    this(Expr object, string fieldName, Expr value) {
+        super(object, fieldName, value);
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put(_fieldName);
+        sb.put("=");
+        _value.toStr(sb);
+    }
 }
 
 class CallExpr : Expr {
-
+    Expr _callee;
+    Expr[] _args;
+    this(Expr callee, Expr[] args) {
+        _callee = callee;
+        _args = args;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _callee.toStr(sb);
+        sb.put("(");
+        bool first = true;
+        foreach(expr; _args) {
+            if (!first)
+                sb.put(",");
+            expr.toStr(sb);
+            first = false;
+        }
+        sb.put(")");
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _callee.accept(visitor);
+        foreach(expr; _args) {
+            expr.accept(visitor);
+        }
+        visitor.exit(this);
+    }
 }
 
 class NewExpr : Expr {
-
+    TypeExpr _typeExpr;
+    Expr _len;
+    Expr _initValue;
+    this(TypeExpr typeExpr) {
+        _typeExpr = typeExpr;
+        _len = null;
+        _initValue = null;
+    }
+    this(TypeExpr typeExpr, Expr len, Expr initValue) {
+        _typeExpr = typeExpr;
+        _len = len;
+        _initValue = initValue;
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put("new ");
+        _typeExpr.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _typeExpr.accept(visitor);
+        if (_len !is null) {
+            _len.accept(visitor);
+            if (_initValue !is null)
+                _initValue.accept(visitor);
+        }
+        visitor.exit(this);
+    }
 }
 
 class InitExpr : Expr {
-
+    NewExpr _newExpr;
+    Expr[] _initExprList;
+    this(NewExpr newExpr, Expr[] initExprList) {
+        _initExprList = initExprList;
+        _newExpr = newExpr;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _newExpr.toStr(sb);
+        sb.put("{");
+        bool first = true;
+        foreach (expr; _initExprList) {
+            if (!first)
+                sb.put(", ");
+            first = false;
+            expr.toStr(sb);
+        }
+        sb.put("}");
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _newExpr.accept(visitor);
+        foreach (expr; _initExprList) {
+            expr.accept(visitor);
+        }
+        visitor.exit(this);
+    }
 }
 
-abstract class Stmt : AST {
-
-}
+abstract class Stmt : AST {}
 
 class IfElseStmt : Stmt {
-
+    Expr _condition;
+    Stmt _ifStmt;
+    Stmt _elseStmt;
+    this(Expr expr, Stmt ifStmt, Stmt elseStmt) {
+        _condition = expr;
+        _ifStmt = ifStmt;
+        _elseStmt = elseStmt;
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put("if(");
+        _condition.toStr(sb);
+        sb.put(")\n");
+        _ifStmt.toStr(sb);
+        if (_elseStmt !is null) {
+            sb.put("\nelse\n");
+            _elseStmt.toStr(sb);
+        }
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _condition.accept(visitor);
+        _ifStmt.accept(visitor);
+        if (_elseStmt !is null)
+            _elseStmt.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class WhileStmt : Stmt {
-
+    Expr _condition;
+    Stmt _stmt;
+    this(Expr expr) {
+        _condition = expr;
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put("while(");
+        _condition.toStr(sb);
+        sb.put(")\n");
+        _stmt.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _condition.accept(visitor);
+        _stmt.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class BreakStmt : Stmt {
-
+    WhileStmt _whileStmt;
+    this(WhileStmt whileStmt) {
+        _whileStmt = whileStmt;
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put("break");
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        visitor.exit(this);
+    }
 } 
 
 class ContinueStmt : Stmt {
-
+    WhileStmt _whileStmt;
+    this(WhileStmt whileStmt) {
+        _whileStmt = whileStmt;
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put("continue");
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        visitor.exit(this);
+    }
 }
 
 class ReturnStmt : Stmt {
-
+    Expr _expr;
+    this(Expr expr) {
+        _expr = expr;
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put("return");
+        if (_expr !is null) {
+            sb.put(" ");
+            _expr.toStr(sb);
+        }
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        if (_expr !is null)
+            _expr.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class AssignStmt : Stmt {
-
+    NameExpr _nameExpr;
+    Expr _rhs;
+    this(NameExpr nameExpr, Expr rhs) {
+        _nameExpr = nameExpr;
+        _rhs = rhs;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _nameExpr.toStr(sb);
+        sb.put(" = ");
+        _rhs.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _nameExpr.accept(visitor);
+        _rhs.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class VarStmt : Stmt {
+    string _varName;
+    VarSymbol _symbol;
+    Expr _expr;
 
+    this(string symbol, Expr expr) {
+        _varName = symbol;
+        _expr = expr;
+    }
+    override void toStr(ref Appender!string sb) const {
+        sb.put("var ");
+        sb.put(_varName);
+        sb.put(" = ");
+        _expr.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _expr.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class ExprStmt : Stmt {
-
+    Expr _expr;
+    this(Expr expr) {
+        _expr = expr;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _expr.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _expr.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class VarDeclStmt : Stmt {
-
+    VarDecl _varDecl;
+    this(VarDecl varDec) {
+        _varDecl = varDec;
+    }
+    override void toStr(ref Appender!string sb) const {
+        _varDecl.toStr(sb);
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        _varDecl.accept(visitor);
+        visitor.exit(this);
+    }
 }
 
 class BlockStmt : Stmt {
-
+    Stmt[] _stmtList;
+    Scope _scope;
+    override void toStr(ref Appender!string sb) const {
+        sb.put("{\n");
+        foreach (stmt; _stmtList) {
+            stmt.toStr(sb);
+            sb.put("\n");
+        }
+        sb.put("}\n");
+    }
+    override void accept(ASTVisitor visitor) {
+        visitor = visitor.enter(this);
+        if (visitor is null)
+            return;
+        foreach (stmt; _stmtList) {
+            stmt.accept(visitor);
+        }
+        visitor.exit(this);
+    }
 }
